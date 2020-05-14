ec4ed3c6628e08caeb64bee22ad306fcdace2b7a
Michael Romay <amerror@gmail.com>
2020-05-13 18:48:30 -0700

    fixed memesReducer test

--- src/reducers/memesReducer.test.js
+++ src/reducers/memesReducer.test.js
@@ -1,15 +1,24 @@
 import reducer from './memesReducer';
-import { addMeme, setMemes } from '../actions/memeActions';
+import { addMeme, setMemes, SET_MEMES, ADD_MEME } from '../actions/memeActions';
 
 describe('memes reducer', () => {
   it('handle the ADD_MEME action', () => {
     const state = [];
-    const action = addMeme({
-      _id: '12345',
-      top: 'This is a thing',
-      image: 'http://placekitten.com/100/100',
-      bottom: 'This is another thing'
-    });
+    const action = {
+      type: ADD_MEME,
+      payload: {
+        _id: '12345',
+        top: 'This is a thing',
+        image: 'http://placekitten.com/100/100',
+        bottom: 'This is another thing'
+      }
+    };
+    // const action = addMeme({
+    //   _id: '12345',
+    //   top: 'This is a thing',
+    //   image: 'http://placekitten.com/100/100',
+    //   bottom: 'This is another thing'
+    // });
     console.log(action);
     const newState = reducer(state, action);
 
@@ -25,12 +34,21 @@ describe('memes reducer', () => {
 
   it('handles the SET_MEMES action', () => {
     const state = [];
-    const action = setMemes([{
-      _id: '12345',
-      top: 'This is a thing',
-      image: 'http://placekitten.com/100/100',
-      bottom: 'This is another thing'
-    }]);
+    const action = {
+      type: SET_MEMES,
+      payload: [{
+        _id: '12345',
+        top: 'This is a thing',
+        image: 'http://placekitten.com/100/100',
+        bottom: 'This is another thing'
+      }]
+    };
+    // const action = setMemes([{
+    //   _id: '12345',
+    //   top: 'This is a thing',
+    //   image: 'http://placekitten.com/100/100',
+    //   bottom: 'This is another thing'
+    // }]);
 
     const newState = reducer(state, action);
 
