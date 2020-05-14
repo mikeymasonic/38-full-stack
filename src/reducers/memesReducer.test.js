3e3b4af88ced60c8abf1c469fb2880dd3a10bda0
Michael Romay <amerror@gmail.com>
2020-05-13 20:14:26 -0700

    clean up

--- src/reducers/memesReducer.test.js
+++ src/reducers/memesReducer.test.js
@@ -1,5 +1,5 @@
 import reducer from './memesReducer';
-import { addMeme, setMemes, SET_MEMES, ADD_MEME } from '../actions/memeActions';
+import { SET_MEMES, ADD_MEME } from '../actions/memeActions';
 
 describe('memes reducer', () => {
   it('handle the ADD_MEME action', () => {
@@ -13,13 +13,7 @@ describe('memes reducer', () => {
         bottom: 'This is another thing'
       }
     };
-    // const action = addMeme({
-    //   _id: '12345',
-    //   top: 'This is a thing',
-    //   image: 'http://placekitten.com/100/100',
-    //   bottom: 'This is another thing'
-    // });
-    console.log(action);
+
     const newState = reducer(state, action);
 
     expect(newState).toEqual([
@@ -43,12 +37,6 @@ describe('memes reducer', () => {
         bottom: 'This is another thing'
       }]
     };
-    // const action = setMemes([{
-    //   _id: '12345',
-    //   top: 'This is a thing',
-    //   image: 'http://placekitten.com/100/100',
-    //   bottom: 'This is another thing'
-    // }]);
 
     const newState = reducer(state, action);
 
--- src/store.js
+++ src/store.js
@@ -2,30 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-// function middleware(store) {
-//   return function (next) {
-//     return function(action) {
-//       // logic
-//     }
-//   }
-// }
-
-// const asyncActionMiddleware = store => next => action => {
-//   // store - this is the redux store (getState and dispatch props)
-//   // next - is our way to move to the next middleware
-//   // action - is the action that was dispatched
-
-//   // be normal
-//   if(typeof action !== 'function') return next(action);
-
-//   action(store.dispatch);
-// };
-
-// const logger = store => next => action => {
-//   console.log(`${action.type} was dispatched with a payload of ${action.payload}`);
-//   next(action);
-// };
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
